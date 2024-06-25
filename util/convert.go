4325cf66d9dd2ffa62422d726b9e02622c0bbedb
barton <lichenghao0403@gmail.com>
2024-06-25 21:32:22 +0800

    convert func

--- util/convert.go
+++ util/convert.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"reflect"
 	"strconv"
+	"time"
 )
 
 func ToString(value interface{}) (string, error) {
@@ -36,5 +37,48 @@ func ToInt64(value interface{}) (int64, error) {
 		return int64(value.(float64)), nil
 	case reflect.String:
 		return strconv.ParseInt(value.(string), 10, 64)
+	}
+}
+
+func ToMonth(value int) (m time.Month, e error) {
+	switch value {
+	case 1:
+		m = time.January
+		return
+	case 2:
+		m = time.February
+		return
+	case 3:
+		m = time.March
+		return
+	case 4:
+		m = time.April
+		return
+	case 5:
+		m = time.May
+		return
+	case 6:
+		m = time.June
+		return
+	case 7:
+		m = time.July
+		return
+	case 8:
+		m = time.August
+		return
+	case 9:
+		m = time.September
+		return
+	case 10:
+		m = time.October
+		return
+	case 11:
+		m = time.November
+		return
+	case 12:
+		m = time.December
+		return
+	default:
+		return m, errors.New("value is not month number")
 	}
 }
